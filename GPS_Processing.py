546563d2ee2c0eeed69e7105aed85d67f0b9b2a0
AgentDoodzik <115586234+AgentDoodzik@users.noreply.github.com>
2024-07-17 01:41:13 +0200

    Initial commit

---
+++ GPS_Processing.py
@@ -0,0 +1,23 @@
+import serial
+import pynmea2
+import time
+import string
+import json
+
+outputDict = {}
+ser=serial.Serial(port="dev/ttyTSH0", baudrate=9600)
+with open("GPSdata.json", 'w') as OutputFile:
+    while True:
+        newline=ser.readline().decode("utf-8").split()
+        parsedLine=pynmea2.parse(newline)
+        if parsedLine.find("GPRMC") > -1:
+            outputDict['latitude']=parsedLine.latitude
+            outputDict['longitude']=parsedLine.longitude
+            json.dump(outputDict,OutputFile)
+            
+            
+            
+        
+
+
+    
